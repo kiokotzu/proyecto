1ceb9ff40312677e6da556b965c553a2af2dfebb
Esteban vera <minos_93105@hotmail.com>
2014-04-02 12:29:13 -0500

    tercera clase 1.1

--- header.php
+++ header.php
@@ -1,10 +1,34 @@
-<header>
-	<div class="logo">
-		<figure>
-			<img src="imgs/logo.png" height="76" width="244" alt="" title="Sfotipy">
-		</figure>
+<header class="clearfix">
+	<div class="logo clearfix">
+		<section class="wrap">
+			<figure class="cont-logo left">
+			<img src="imgs/logo.png" width="122" alt="" title="Sfotipy">
+			</figure>
+			<div class="usuario right">
+				<figure class="avatar left">
+					<img src="imgs/avatar.jpg" width="38" height="38" alt="">
+				</figure>
+				<span class="nikname">@Esteban</span>
+			</div>
+			<div class="buscador right">
+				<form action="" class="relative">
+					<span class="icon-search search-icon"></span>
+					<input type="text" placeholder="Buscar">
+					<span class="close-icon">x</span>
+				</form>
+			</div>
+		</section>
 	</div>
 	<a href="" class="button-menu icon-menu"></a>
+	<nav class="clearfix">
+		<ul class="wrap">
+			<li><a href="">Home</a></li>
+			<li><a href="">Nosotros</a></li>
+			<li><a href="">Servicios</a></li>
+			<li><a href="">Productos</a></li>
+			<li><a href="">Contáctenos</a></li>
+		</ul>
+	</nav>
 	<div class="titulo">
 		<span>
 			Tomalo
